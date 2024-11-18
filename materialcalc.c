e1b4e75f657250463da82955461b15c98fdb0dfe
Alpfury <101234721+Alphurion@users.noreply.github.com>
2024-11-17 16:43:00 -0800

    Create materialcalc.c

---
+++ materialcalc.c
@@ -0,0 +1,33 @@
+/*
+Calculates the materials necessary to make projects and saves to a file
+
+
+
+TODO:
+* Create a function that reads in from the console
+* Function to remember materials needed
+* Add unknown materials calculator
+* All basic materials to refined materials, so a refine function basically
+* 
+
+Materials
+Iron, Nickel, Silicon, Gravel, Cobalt, Silver, Gold, Magnesium, Platinum, Uranium
+
+Crafted Materials
+Steel Plates, Metal Grids, Construction Components, Motors,  Small Steel Tubes, Large Steel Tubes
+Solar Cells, Gravity Components, Superconductors
+
+FOR FUN: Calculate the minimum amount of rocks you need to create a project
+*/
+
+
+void materialConversion(){}
+
+#include <stdio.h>
+#include <stdbool.h>
+int main()
+{
+    bool yes = true;
+    bool no = false;
+    printf("%d", 10 < 9);
+}
